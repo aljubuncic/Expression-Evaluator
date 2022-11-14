73da1f26f76378c0a4a6350eec24190adf7677cd
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-11-14 21:03:42 +0100

    Implemented the method validate and a few tests for the same method, deleted the InvalidExpressionException class, added Maven javadoc plugin 3.4.1

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -9,8 +9,70 @@ public class ExpressionEvaluator {
     private Stack<String> operatorStack;
 
     /**
-     * Initializes the expression with the argument provided.
-     * Throws RunTimeException if the expression provided in the argument is not valid
+     * checks if expression is parenthesized from the outer sides
+     */
+    private void isParenthesizedFromOuterSides(String expression){
+        if(expression.charAt(0) != '(' || expression.charAt(expression.length()-1)!= ')')
+            throw new RuntimeException();
+    }
+
+    /**
+     * checks if expression contains characters that are not "(", ")","+","-","*","/","sqrt"
+     */
+
+    private void doesContainInvalidCharacters(String expression){
+        if(!expression.contains("(") || !expression.contains(")") ||
+                !(expression.contains("+") || expression.contains("-")
+                        || expression.contains("*") || expression.contains("/")
+                        || expression.contains("sqrt")))
+            throw new RuntimeException();
+    }
+
+    /**
+     * checks if every segment (parenthesis, operator or operand) of expression
+     * is separated from each other by a space character
+     */
+    private void isEverySegmentOfExpressionSeparatedCorrectly(ArrayList<String> stringsSeparatedBySpace) {
+        for (String s : stringsSeparatedBySpace) {
+            try {
+                Integer.parseInt(s);
+            } catch (NumberFormatException n) {
+                if (s.length() != 1 && !s.equals("sqrt"))
+                    throw new RuntimeException();
+            }
+        }
+    }
+
+    /**
+     * checks if expression includes at least two numbers
+     */
+    private void doesHaveAtLeastTwoNumbers(ArrayList<String> stringsSeparatedBySpace) {
+        int counterOfNumbers = 0;
+        for (String s : stringsSeparatedBySpace) {
+            if (Character.isDigit(s.charAt(0)))
+                counterOfNumbers++;
+        }
+        if (counterOfNumbers < 2)
+            throw new RuntimeException();
+    }
+
+    /**
+     * checks if expression has equal number of right and left parenthesis
+     */
+    private void doesHaveEqualNumberOfLeftAndRightParenthesis(ArrayList<String> stringsSeparatedBySpace){
+        Integer numOfLeftParenthesis=0;
+        Integer numOfRightParenthesis=0;
+        for(String s : stringsSeparatedBySpace){
+            if(s.equals("("))
+                numOfLeftParenthesis++;
+            if(s.equals(")"))
+                numOfRightParenthesis++;
+        }
+        if(!numOfLeftParenthesis.equals(numOfRightParenthesis))
+            throw new RuntimeException();
+    }
+    /**
+     * Initializes stacks for operands and operators to empty stacks.
      */
 
     public ExpressionEvaluator(){
@@ -25,42 +87,18 @@ public class ExpressionEvaluator {
     public Double evaluate(String expression){
         Double result =null;
         for(String s : expression.split(" ")){
-            //to be implemented
+
         }
         return result;
     }
-    public void validate(String expression) throws InvalidExpressionException{
-        /**
-         * checks if expression contains characters that are not "(", ")","+","-","*","/","sqrt"
-         */
-        String numbers="0123456789";
-        if(!expression.contains("(") || !expression.contains(")") ||
-                !(expression.contains("+") || expression.contains("-")
-                        || expression.contains("*") || expression.contains("/")
-                        || expression.contains("sqrt")))
-            throw new InvalidExpressionException();
+    public void validate(String expression) {
+        isParenthesizedFromOuterSides(expression);
+        doesContainInvalidCharacters(expression);
 
-        ArrayList<String> oneCharacterStringArray= new ArrayList<>(Arrays.asList( expression.split(" ")));
-        /**
-         * checks if every character of expression is split by a space
-         */
-        //String[] characterArray=expression.split(" ");
-        for(String s : oneCharacterStringArray){
-            if(s.length()!=1)
-                throw new InvalidExpressionException();
-        }
-        /**
-         * check if expression has equal number of right and left parenthesis
-         */
-        Integer numOfLeftParenthesis=0;
-        Integer numOfRightParenthesis=0;
-        for(String s : oneCharacterStringArray){
-            if(s.equals("("))
-                numOfLeftParenthesis++;
-            if(s.equals(")"))
-                numOfRightParenthesis++;
-        }
-        if(!numOfLeftParenthesis.equals(numOfRightParenthesis))
-            throw new InvalidExpressionException();
-        }
+        ArrayList<String> stringsSeparatedBySpace = new ArrayList<>(Arrays.asList(expression.split(" ")));
+
+        isEverySegmentOfExpressionSeparatedCorrectly(stringsSeparatedBySpace);
+        doesHaveAtLeastTwoNumbers(stringsSeparatedBySpace);
+        doesHaveEqualNumberOfLeftAndRightParenthesis(stringsSeparatedBySpace);
+    }
 }
--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -1,9 +1,33 @@
 package ba.unsa.etf.rpr;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
-public class ExpressionEvaluatorTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class ExpressionEvaluatorTest {
+    @Test
+    void validateTest1(){
+        assertThrows(java.lang.RuntimeException.class, () -> new ExpressionEvaluator().validate(""));
+    }
+    @Test
+    void validateTest2(){
+        assertThrows(RuntimeException.class, () -> new ExpressionEvaluator().validate("5*3"));
+    }
+    @Test
+    void validateTest3(){
+        assertThrows(RuntimeException.class, () -> new ExpressionEvaluator().validate("( 5 *3 )"));
+    }
+
+    @Test
+    void validateTest4(){
+        assertDoesNotThrow(() -> new ExpressionEvaluator().validate("( sqrt ( 5 ) * 3 )"));
+    }
+    @Test
+    void validateTest5(){
+        assertThrows(RuntimeException.class, () -> new ExpressionEvaluator().validate("( f * 3 )"));
+    }
+    @Test
+    void validateTest6(){
+        assertThrows(RuntimeException.class, ()-> new ExpressionEvaluator().validate("( 5 * ( 3 + 2 )"));
+    }
 }
